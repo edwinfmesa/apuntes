6a745c6938588a8b8ef07273ff260adf9e42e67a
Edwin Mesa <edwinfmesa@hotmail.com>
2012-06-28 17:53:42 -0500

    Agregando un modelo

---
+++ web/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from web.models import generales
+
+#registramos los comentarios
+admin.site.register(generales)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class generales(models.Model):
+    titulo = models.CharField(max_length = 20, verbose_name="Titulo")
+    fecha = models.DateTimeField()
+    texto = models.TextField(blank = True)
+    hola ="hola mundo" 
+    
+    def __unicode__(self):
+        return "%s "%(self.titulo)
