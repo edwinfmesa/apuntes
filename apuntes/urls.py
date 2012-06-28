0e5e82d46d6100334ef38e8297a288d80d046051
Edwin Mesa <edwinfmesa@hotmail.com>
2012-06-27 22:11:35 -0500

    Aprendiendo a manejar los templates de django
    -template/base.html es la plantilla principal o lo que era el layout en
    ZF para utilizarla, por ejemplo home.html hereda de base.html y define
    algo asi como sus bloques de codigo correspondientes.
    
    -Se agragaron URLs al proyecto, se encuentran en menu.html el cual es
    incluido desde base.html
    
    -Se incluyo el CSS de html5boilerplate para setear los estilos
    
    -Se agregaron las librerias JQuery y Modernizer al proyecto
    
    -Se estan imprimiendo variables que llegan por GET a enlaces.html

--- apuntes/urls.py
+++ apuntes/urls.py
@@ -5,11 +5,14 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'web.views.home'),
+    url(r'^enlaces/?$', 'web.views.enlaces2'),
+    url(r'^enlaces/(?P<var>.+?)/?$', 'web.views.enlaces'),
+    url(r'^instalaciones/', 'web.views.instalaciones'),
     # url(r'^apuntes/', include('apuntes.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)), #incluye las urls de la aplicacion
 )
--- web/views.py
+++ web/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render_to_response
 
+#funcion basica que recibe una solicitud y carga un html
 def home(request):
     return render_to_response('web/home.html', None)
 
+def enlaces(request,var):
+#    if var != None:
+#        contexto = {'nombre':var,'apellido': 'mesa salazar'}
+#    else:
+    contexto = {'nombre':var,'apellido': 'mesa salazar'}
+    return render_to_response('web/enlaces.html', contexto )
+
+def enlaces2(request):
+    return render_to_response('web/enlaces.html')
+
+def instalaciones(request):
+    return render_to_response('web/instalaciones.html', None)
