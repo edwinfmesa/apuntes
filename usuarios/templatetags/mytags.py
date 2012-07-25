4d1d573f08176692cca99a438813768652128a23
Edwin Mesa <edwinfmesa@hotmail.com>
2012-07-24 23:49:48 -0500

    intento 4

--- usuarios/templatetags/mytags.py
+++ usuarios/templatetags/mytags.py
@@ -4,7 +4,7 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('templatetags/gravatar.html')
-def show_gravatar(email, size=48):
+def showgravatar(email, size=48):
     default = "http://cms.myspacecdn.com/cms/Music%20Vertical/Common/Images/default_small.jpg"
 
     url = "http://www.gravatar.com/avatar.php?"
