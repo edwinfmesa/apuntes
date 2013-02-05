535b42471bb357859b8faa71bc4d8dee19e1cec7
Edwin Mesa <edwinfmesa@hotmail.com>
2013-02-05 10:40:17 -0500

    retomando proyecto apuntes

--- static/js/textareas.js
+++ static/js/textareas.js
@@ -4,11 +4,11 @@ tinyMCE.init({
 	theme : "advanced",
 	extended_valid_elements: "textarea[name|class|cols|rows]", //code
 	remove_linebreaks : false, //code
-	plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave,codesyntax, syntaxhl, bbcode",
+	plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave,codesyntax, syntaxhl",
  
 	// Theme options
 	theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect", //,fontsizeselect,fullscreen,code|,codeformat",
-	theme_advanced_buttons2 : " syntaxhl ,bbcode|,cut,copy,paste,pastetext,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup", //,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+	theme_advanced_buttons2 : " syntaxhl|,cut,copy,paste,pastetext,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup", //,|,insertdate,inserttime,preview,|,forecolor,backcolor",
 	theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions", //,iespell,media,advhr,|,print,|,ltr,rtl",
  
 	theme_advanced_toolbar_location : "top",
